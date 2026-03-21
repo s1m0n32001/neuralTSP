dc2d9c7173778242eaf29dfbb64e0bd1d465fd1f
Simone <simonetoso2001@gmail.com>
2026-03-21 02:55:33 +0100

    import bug fix

--- neuraltsp/model/__init__.py
+++ neuraltsp/model/__init__.py
@@ -1,3 +1,5 @@
-from neuraltsp.model.attention import TriangularAttentionContext
+from neuraltsp.model.model import TSPTransformer
+from neuraltsp.model.candidates import build_candidate_indices
+from neuraltsp.model.decode import predict_tour, tour_length
 
-__all__ = ["TriangularAttentionContext"]
+__all__ = ["TSPTransformer", "build_candidate_indices", "predict_tour", "tour_length"]
